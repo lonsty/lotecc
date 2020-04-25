7318e0aa6831d050a6520d8082398d66b36f0905
Allen Shaw <lonsty@sina.com>
2020-04-25 20:13:55 +0800

    test: test usage from module

--- tests/test_lotecc.py
+++ tests/test_lotecc.py
@@ -36,7 +36,7 @@ def test_command_line_interface():
     assert 'Show this message and exit.' in help_result.output
 
 
-def test_s2t():
+def test_command_line_s2t():
     """Test s2t."""
 
     input_file = 'tests/testcases/s2t.in'
@@ -62,7 +62,7 @@ def test_s2t():
         pass
 
 
-def test_t2s():
+def test_command_line_t2s():
     """Test t2s."""
 
     input_file = 'tests/testcases/t2s.in'
@@ -88,7 +88,7 @@ def test_t2s():
         pass
 
 
-def test_lote_s2t():
+def test_command_line_lote_s2t():
     """Test lote s2t"""
 
     input_dir = 'tests/testcases'
@@ -96,13 +96,16 @@ def test_lote_s2t():
         'tests/testcases/s2t_c.ans',
         'tests/testcases/s2t_c.in',
         'tests/testcases/t2s_c.ans',
-        'tests/testcases/t2s_c.in'
+        # 'tests/testcases/t2s_c.in',
+        ''
     ]
     answer_files = [
-        'tests/testcases/s2t.ans',
+        # 'tests/testcases/s2t.ans',
+        '',
         'tests/testcases/s2t.ans',
         'tests/testcases/t2s.in',
-        'tests/testcases/t2s.in'
+        # 'tests/testcases/t2s.in',
+        '',
     ]
 
     runner = CliRunner()
@@ -110,15 +113,34 @@ def test_lote_s2t():
     assert conversion_result.exit_code == 0
 
     for idx, output_file in enumerate(output_files):
-        assert os.path.isfile(output_file)
+        answer_file = answer_files[idx]
+        if output_file and answer_file:
+            assert os.path.isfile(output_file)
 
-        # with open(output_file, 'r') as f:
-        #     output_str = f.read().strip()
-        # with open(answer_files[idx], 'r') as f:
-        #     answer_str = f.read().strip()
-        #
-        # assert output_str == answer_str
+            with open(output_file, 'r') as f:
+                output_str = f.read().strip()
+            with open(answer_file, 'r') as f:
+                answer_str = f.read().strip()
 
+            assert output_str == answer_str
+
+        try:
+            os.remove(output_file)
+        except FileNotFoundError:
+            pass
+
+
+def test_lotecc_s2t():
+    ret = lotecc.lote_chinese_conversion(conversion='s2t',
+                                         input='tests/testcases/',
+                                         output=None,
+                                         in_enc='utf-8',
+                                         out_enc='utf-8',
+                                         suffix='_c',
+                                         ignore='.gitignore')
+    assert len(ret) == 3
+
+    for _, output_file in ret:
         try:
             os.remove(output_file)
         except FileNotFoundError:
